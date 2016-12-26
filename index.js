354c2d41feef0881e5206cdba86648e5ce7261ca
kevinjyee <kevjyee@gmail.com>
2016-12-26 12:21:54 -0600

    updated response token

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('1624716528')
 })
 
 // for Facebook verification
