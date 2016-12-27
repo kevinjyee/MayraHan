923f27853b7e12c2b9d54199c28fe5cc0c5cd8af
kevinjyee <kevjyee@gmail.com>
2016-12-26 20:52:26 -0600

    fixed broken db

--- setup/init.js
+++ setup/init.js
@@ -7,29 +7,7 @@ var HashMap = require("hashmap");
 
 var map = new HashMap();
 
-//Initialize data base
-var firebase = require("firebase");
-var config = {
-    apiKey: "AIzaSyDTrJWeSWmX5JTPW-h0rX1d0NAqntSnF8k",
-    authDomain: "mayra-han.firebaseapp.com",
-    databaseURL: "https://mayra-han.firebaseio.com",
-    storageBucket: "mayra-han.appspot.com",
-    messagingSenderId: "819633055429"
-};
-firebase.initializeApp(config);
 
-// Get a reference to the database service
-
-var database = firebase.database();
-
-function writeUserData(userId, name, email) {
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email
-    });
-}
-
-writeUserData(1, 'hashim', 'hh1316');
 
 
 exports.map = map;
