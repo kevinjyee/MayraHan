41416bb4e18b3d3fe67b4bbe88568c20a6208293
kevinjyee <kevjyee@gmail.com>
2016-12-28 19:54:14 -0600

    added FileLink

--- index.js
+++ index.js
@@ -103,7 +103,8 @@ app.post('/webhook/', function (req, res) {
                     ref.on("value", function(snapshot) {
                         console.log(snapshot.val());
 
-                        sendTextMessage(sender, "Here is your file link" + snapshot.val().FileLink);
+                        sendTextMessage(sender, "Here is your file link test 1" + snapshot.val().FileLink);
+                        sendTextMessage(sender, "here is your file link test 2" + snapshot.child("FileLink").val());    
                     }, function (error) {
                         sendTextMessage(sender, "Here is your error" + error);
                     });
