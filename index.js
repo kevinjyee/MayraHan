6b6aa607ab5276c3e154c4cd2f64f36bc730a4cb
kevinjyee <kevjyee@gmail.com>
2016-12-28 19:16:41 -0600

    added ref Key

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post('/webhook/', function (req, res) {
                     ref.on("value", function(snapshot) {
                         console.log(snapshot.val());
 
-                        sendTextMessage(sender, "Here is your file link" + snapshot.child("FileLink").val());
+                        sendTextMessage(sender, "Here is your file link" + snapshot.child(String(ref.getKey())).val());
                     }, function (error) {
                         sendTextMessage(sender, "Here is your error" + error);
                     });
